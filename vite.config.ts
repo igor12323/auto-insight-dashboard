9e104be00e0863fd9d1709633faf00913d0f0ed8
igor12323 <igorg12323@gmail.com>
2025-05-14 14:15:16 +0200

    v0.3

--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default {
+  server: {
+    host: '0.0.0.0', // Umożliwia dostęp spoza localhost
+    port: 3000, // Możesz zmienić port
+  }
+}
